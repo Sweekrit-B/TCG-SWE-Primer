ea4afb160db0731b934ce6df1cfa2f1d42fa7af2
Sweekrit-B <sweekrit.bhatnagar@gmail.com>
2026-03-25 16:56:27 -0700

    complete backend setup and supabase initial connection

---
+++ backend/src/app.ts
@@ -0,0 +1,40 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { createClient } from "@supabase/supabase-js";
+
+// Load environment variables from .env file
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+// Create Express app
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  }),
+);
+
+app.use(express.json());
+
+// Initialize Supabase client
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+
+if (!supabaseUrl || !supabaseServiceRoleKey) {
+    throw new Error("Supabase environment variables are not set properly.");
+}
+
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+if (!supabase) {
+    throw new Error("Failed to initialize Supabase client.");
+} else {
+    console.log("Supabase client initialized successfully.");
+    // Start the server and listen on the specified port
+    app.listen(process.env.BACKEND_PORT || 4000, () => {
+        console.log(`Server is running on port ${process.env.BACKEND_PORT || 4000}`);
+    });
+}
