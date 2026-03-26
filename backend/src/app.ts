accb67c217f23cd8526fe6b4b90d8131da9a6012
Sweekrit-B <sweekrit.bhatnagar@gmail.com>
2026-03-25 17:37:45 -0700

    minor changes to backend/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,7 +23,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/user", userRoutes);\
+app.use("/user", userRoutes);
 app.use("/project", projectRoutes);
 app.use("/associates", associateRoutes);
 
